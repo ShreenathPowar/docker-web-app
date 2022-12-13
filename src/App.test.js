5c749a88e51c33cec5849a6af31cf7487ec9d864
shreenathpowar <shreenath.powar@gmail.com>
2022-12-14 01:27:39 +0530

    changed url name

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn the best frontend library ever exists/i);
   expect(linkElement).toBeInTheDocument();
 });
 
